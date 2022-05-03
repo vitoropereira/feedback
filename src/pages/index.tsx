0f1fbd3ed1e0fe3b1ab1743bfb1a6daa3b7584f3
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2022-05-02 23:23:06 -0300

    Start Project

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { Widget } from "../components/Widget";
+
+const Home: NextPage = () => {
+  return <Widget />;
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    500: "#8257e6",
+                },
+            },
+        },
+    },
+    plugins: [],
+};
